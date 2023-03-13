0b88c5a68c9b531fddf686deeacfd6a8b9aaf617
Riya <hiyoriorria@gmail.com>
2023-03-13 18:01:54 +0900

    記事の追加

--- test/db4fb02ecf57a6/test.ts
+++
@@ -1,7 +0,0 @@
-const getDecimalLength = (value: number) => {
-    let x = (value + '').split('.')[1];
-    if (typeof x !== 'undefined' && x.length > 0) {
-        return x.length;
-    }
-    return 0;
-};
